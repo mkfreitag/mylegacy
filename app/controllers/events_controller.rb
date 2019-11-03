3ed3abcebac25d3edf4a72bb4864beb44659a04c
Mike Freitag <mkfreitag@gmail.com>
2019-11-03 01:01:16 +0000

    show new page and add create action 4

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,21 @@
 class EventsController < ApplicationController
 
+  def new
+    @event = Event.new
+  end
+
   def index
+  end
+
+  def create
+    @event = Event.create(event_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title)
   end
   
 end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,10 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
-  describe "devents#index" do
-    it "should successflly show the page" do
+  describe "devents#index action" do
+    it "should successfully show the page" do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "event#index action" do
+    it "should successfully show the new form" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "events#create action" do
+    it "should successfully create a new event in our database" do
+      post :create, params: { event: { title: 'Hello!' } }
+      expect(response).to redirect_to root_path
+
+      event = Event.last
+      expect(event.title).to eq("Hello!")
+    end
+  end
+
 end
